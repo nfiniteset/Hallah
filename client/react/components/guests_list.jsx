44781bfdb2c523a90f050b633b401bf3b8b590bb
Sean Durham <sean@sdurham.net>
2018-11-23 10:35:10 -0800

    Upgrade Meteor to 1.8

--- client/react/components/guests_list.jsx
+++ client/react/components/guests_list.jsx
@@ -1,19 +1,9 @@
 import React, { PropTypes } from 'react';
 
-GuestsList = React.createClass({
-  renderGuests() {
-    return this.props.guests.map((guest) => {
-      return (
-        <GuestHistoryItem id={guest._id} name={guest.name} key={guest.name}/>
-      )
-    });
-  },
-
-  render() {
-    return (
-      <ul>
-        {this.renderGuests()}
-      </ul>
-    )
-  }
-});
+GuestsList = function GuestsList(props) {
+  return (
+    <ul>
+      {props.guests.map(guest => <GuestHistoryItem id={guest._id} name={guest.name} key={guest.name}/>)}
+    </ul>
+  )
+};
